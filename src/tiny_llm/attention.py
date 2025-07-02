ff47021f73754a359578ceb15aa80c2f7cf418af
HuapengZhou <zhouhp.me@gmail.com>
2025-07-01 21:17:12 -0400

    Impl week1 day1

--- src/tiny_llm/attention.py
+++ src/tiny_llm/attention.py
@@ -9,7 +9,11 @@ def scaled_dot_product_attention_simple(
     scale: float | None = None,
     mask: mx.array | None = None,
 ) -> mx.array:
-    pass
+    scale = mx.rsqrt(query.shape[-1]) if scale is None else scale
+    logits = mx.matmul(query, key.swapaxes(-1, -2)) * scale
+    if mask is not None:
+        logits = logits + mask
+    return mx.matmul(softmax(logits, axis=-1), value)
 
 
 class SimpleMultiHeadAttention:
@@ -22,7 +26,14 @@ class SimpleMultiHeadAttention:
         wv: mx.array,
         wo: mx.array,
     ):
-        pass
+        self.wq = wq
+        self.wk = wk
+        self.wv = wv
+        self.wo = wo
+        self.num_heads = num_heads
+        self.hidden_size = hidden_size
+        self.head_dim = hidden_size // num_heads
+        self.scale = mx.rsqrt(self.head_dim)
 
     def __call__(
         self,
@@ -31,7 +42,25 @@ class SimpleMultiHeadAttention:
         value: mx.array,
         mask: mx.array | None = None,
     ) -> mx.array:
-        pass
+        batch_size, seq_len = query.shape[0], query.shape[1]
+        proj_query = linear(query, self.wq)
+        proj_key = linear(key, self.wk)
+        proj_value = linear(value, self.wv)
+
+        proj_query = proj_query.reshape(batch_size, seq_len, self.num_heads, self.head_dim)
+        proj_key = proj_key.reshape(batch_size, seq_len, self.num_heads, self.head_dim)
+        proj_value = proj_value.reshape(batch_size, seq_len, self.num_heads, self.head_dim)
+
+        proj_query = proj_query.transpose(0, 2, 1, 3)
+        proj_key = proj_key.transpose(0, 2, 1, 3)
+        proj_value = proj_value.transpose(0, 2, 1, 3)
+
+        output = scaled_dot_product_attention_simple(proj_query, proj_key, proj_value, scale=self.scale, mask=mask)
+        output = output.transpose(0, 2, 1, 3)
+        output = output.reshape(batch_size, seq_len, self.hidden_size)
+        output = linear(output, self.wo)
+
+        return output
 
 
 def causal_mask(L: int, S: int, dtype: mx.Dtype) -> mx.array:
